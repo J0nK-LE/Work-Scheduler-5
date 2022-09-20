309e89fd4200153fb2b80b958af7b764b65953c1
Jon Kelly <jonfkelly@gmail.com>
2022-09-20 17:07:49 -0500

    completed ReadMe and formatted files to be turned in

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,30 @@
-# Work Day Scheduler Starter Code
+<h1 align="center">Jon Kelly's Work Day Scheduler's ReadMe</h1>
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+This assignment for this challenge was to create a working Work Day Scheduler that:
+
+* included today time and date at the top of the screen using moment()
+* have each hour change color based on its relation to the current time
+    * the current time is Red
+    * the past hours are Grey
+    * the future hours are Green
+* have each hour have a text area that can be edited
+* have a button attached to each hour that saves that hours text into local storage
+
+
+ 
+<!-- Links -->
+## Links
+
+Deployed application:
+https://j0nk-le.github.io/challenge5/
+
+Project Link:
+https://github.com/J0nK-LE/challenge5
+
+
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-// let saveBtn = $(".saveBtn");
 let saveBtn9 = $("#nineBtn")
 let saveBtn10 = $("#tenBtn")
 let saveBtn11 = $("#elevenBtn")
@@ -20,8 +19,8 @@ let textArea4 = $("#fourP")
 let textArea5 = $("#fiveP")
 
 let currentTime = parseInt(moment().format("H"))
-// let currentTime = 19;
 let element;
+
 let getLocalStorage9 = localStorage.getItem("nineA")
 let getLocalStorage10 = localStorage.getItem("tenA")
 let getLocalStorage11 = localStorage.getItem("elevenA")
@@ -42,82 +41,82 @@ textArea3.text(getLocalStorage3)
 textArea4.text(getLocalStorage4)
 textArea5.text(getLocalStorage5)
 
-setInterval(function() {
-    $("#currentDay").text(moment().format("dddd, MMMM Do [at ] hh:mm:ss a"))    
+setInterval(function () {
+    $("#currentDay").text(moment().format("dddd, MMMM Do [at ] hh:mm:ss a"))
 }, 1000);
 
-saveBtn9.on("click", function (){
-    
-        element = textArea9.val();
-        console.log("text area 9 ", element);
-        localStorage.setItem("nineA", element);
-      })
-    
-saveBtn10.on("click", function (){
-    
-        element = textArea10.val();
-        console.log("text area 10 ", element);
-        localStorage.setItem("tenA", element);
-    
-    })
-    
-saveBtn11.on("click", function (){
-    
-        element = textArea11.val();
-        console.log("text area 11 ", element);
-        localStorage.setItem("elevenA", element);
-    
-    })
-saveBtn12.on("click", function (){
-    
-        element = textArea12.val();
-        console.log("text area 12 ", element);
-        localStorage.setItem("twelveP", element);
-    
-    })
-saveBtn1.on("click", function (){
-    
-        element = textArea1.val();
-        console.log("text area 1 ", element);
-        localStorage.setItem("oneP", element);
-    
-    })
-    
-saveBtn2.on("click", function (){
-    
-        element = textArea2.val();
-        console.log("text area 2 ", element);
-        localStorage.setItem("twoP", element);
-    
-    })
-saveBtn3.on("click", function (){
-    
-        element = textArea3.val();
-        console.log("text area 3 ", element);
-        localStorage.setItem("threeP", element);
-    
-    })
-    
-saveBtn4.on("click", function (){
-    
-        element = textArea4.val();
-        console.log("text area 4 ", element);
-        localStorage.setItem("fourP", element);
-    
-    })
-saveBtn5.on("click", function (){
-    
-        element = textArea5.val();
-        console.log("text area 5 ", element);
-        localStorage.setItem("fiveP", element);
-    
-    })
-    
+saveBtn9.on("click", function () {
 
-    
-    
-function displayTime () {
-        console.log(currentTime);
+    element = textArea9.val();
+    console.log("text area 9 ", element);
+    localStorage.setItem("nineA", element);
+})
+
+saveBtn10.on("click", function () {
+
+    element = textArea10.val();
+    console.log("text area 10 ", element);
+    localStorage.setItem("tenA", element);
+
+})
+
+saveBtn11.on("click", function () {
+
+    element = textArea11.val();
+    console.log("text area 11 ", element);
+    localStorage.setItem("elevenA", element);
+
+})
+saveBtn12.on("click", function () {
+
+    element = textArea12.val();
+    console.log("text area 12 ", element);
+    localStorage.setItem("twelveP", element);
+
+})
+saveBtn1.on("click", function () {
+
+    element = textArea1.val();
+    console.log("text area 1 ", element);
+    localStorage.setItem("oneP", element);
+
+})
+
+saveBtn2.on("click", function () {
+
+    element = textArea2.val();
+    console.log("text area 2 ", element);
+    localStorage.setItem("twoP", element);
+
+})
+saveBtn3.on("click", function () {
+
+    element = textArea3.val();
+    console.log("text area 3 ", element);
+    localStorage.setItem("threeP", element);
+
+})
+
+saveBtn4.on("click", function () {
+
+    element = textArea4.val();
+    console.log("text area 4 ", element);
+    localStorage.setItem("fourP", element);
+
+})
+saveBtn5.on("click", function () {
+
+    element = textArea5.val();
+    console.log("text area 5 ", element);
+    localStorage.setItem("fiveP", element);
+
+})
+
+
+
+
+function displayTime() {
+    console.log(currentTime);
     if (currentTime === 9) {
         $("#nineA").addClass("present")
         $("#tenA").addClass("future")
@@ -228,43 +227,9 @@ function displayTime () {
         $("#fourP").addClass("future")
         $("#fiveP").addClass("future")
     }
-    // if ((!currentTime === "9") || (!currentTime === "10") || (!currentTime === "11") || (!currentTime === "12") || (!currentTime === "13") || (!currentTime === "14") || (!currentTime === "15") || (!currentTime === "16") || (!currentTime === "17")) {
-    //     $("#nineA").addClass("future")
-    //     $("#tenA").addClass("future")
-    //     $("#elevenA").addClass("future")
-    //     $("#twelveP").addClass("future")
-    //     $("#oneP").addClass("future")
-    //     $("#twoP").addClass("future")
-    //     $("#threeP").addClass("future")
-    //     $("#fourP").addClass("future")
-    //     $("#fiveP").addClass("future")
-    // }
-    // else {
-    //     $("#nineA").addClass("future")
-    //     $("#tenA").addClass("future")
-    //     $("#elevenA").addClass("future")
-    //     $("#twelveP").addClass("future")
-    //     $("#oneP").addClass("future")
-    //     $("#twoP").addClass("future")
-    //     $("#threeP").addClass("future")
-    //     $("#fourP").addClass("future")
-    //     $("#fiveP").addClass("future")
-    // }
+
 }
 displayTime()
 
 
 
-
-
-// let buttonSaves = target the button clicked parents 2nd child
-
-// saveBtn.addEventListener("click", function(event){
-    //     console.log(buttonSaves)
-    
-    //     function storeEvents() {
-        //                 localStorage.setItem("events", JSON.stringify(events));
-        //       }
-        // })
-        
-        
